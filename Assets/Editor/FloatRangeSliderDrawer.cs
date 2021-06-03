4ec1406dabd2649a3d1419f3c01005984e7bf5ca
Fanil <gimalfan@gmail.com>
2021-06-03 23:37:51 +0300

    Исправлял движение врагов, не до конца ещё доделал

--- Assets/Editor/FloatRangeSliderDrawer.cs
+++ Assets/Editor/FloatRangeSliderDrawer.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(Fl) ]
+[CustomPropertyDrawer(typeof(FloatRangeSliderAtribute))]
 public class FloatRangeSliderDrawer : PropertyDrawer
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        
-    }
+        int originalIdentLevel = EditorGUI.indentLevel;
+        EditorGUI.BeginProperty(position, label, property);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        EditorGUI.indentLevel = 0;
+        SerializedProperty minProperty = property.FindPropertyRelative("_min");
+        SerializedProperty maxProperty = property.FindPropertyRelative("max");
+        float minValue = minProperty.floatValue;
+        float maxValue = maxProperty.floatValue;
+        float fieldWidth = position.width / 4f - 4f;
+        float sliderWidth = position.width / 2f;
+        position.width = sliderWidth;
+        FloatRangeSliderAtribute limit = attribute as FloatRangeSliderAtribute;
+        EditorGUI.MinMaxSlider(position, ref minValue, ref minValue, limit.Min, limit.Max);
+        position.x += sliderWidth + 4f;
+        position.width = fieldWidth;
+        maxValue = EditorGUI.FloatField(position, maxValue);
+        if (minValue <limit.Min)
+        {
+            minValue = limit.Min;
+        }
+
+        if (maxValue > limit.Max)
+        {
+            maxValue = limit.Max;
+        }
+        else if (maxValue < minValue)
+        {
+            maxValue = minValue;
+        }
+
+        minProperty.floatValue = minValue;
+        maxProperty.floatValue = maxValue;
+        EditorGUI.EndProperty();
+        EditorGUI.indentLevel = originalIdentLevel;
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,6 +13,13 @@ public class Enemy : MonoBehaviour
     private Direction _direction;
     private DirectionChange _directionChange;
     private float _directionAngleFrom, _directionAngleTo;
+    private float _pathOffset;
+
+    public void Initialize(float scale, float pathOffset)
+    {
+        _model.localScale = new Vector3(scale, scale, scale);
+        _pathOffset = pathOffset;
+    }
     public void SpawnOn(GameTile tile)
     {
         transform.localPosition = tile.transform.localPosition;
@@ -29,6 +36,7 @@ public class Enemy : MonoBehaviour
         _direction = _tileFrom.PathDirection;
         _directionChange = DirectionChange.None;
         _directionAngleFrom = _directionAngleTo = _direction.GetAngle();
+        _model.localPosition = new Vector3(_pathOffset, 0f);
         transform.localRotation = _direction.GetRotation();
         _progressFactor = 2f;
     }
@@ -38,7 +46,7 @@ public class Enemy : MonoBehaviour
         _positionTo = _tileFrom.transform.localPosition;
         _directionChange = DirectionChange.None;
         _directionAngleTo = _direction.GetAngle();
-        _model.localPosition = Vector3.zero;
+        _model.localPosition = new Vector3(_pathOffset, 0f);
         transform.localRotation = _direction.GetRotation();
         _progressFactor = 2f;
     }
@@ -103,32 +111,32 @@ public class Enemy : MonoBehaviour
     {
         transform.localRotation = _direction.GetRotation();
         _directionAngleTo = _direction.GetAngle();
-        _model.localPosition = Vector3.zero;
+        _model.localPosition = new Vector3(_pathOffset, 0f);
         _progressFactor = 1f;
     }
 
     private void PrepareTurnRight()
     {
         _directionAngleTo = _directionAngleFrom + 90f;
-        _model.localPosition = new Vector3(-0.5f, 0f);
+        _model.localPosition = new Vector3(_pathOffset - 0.5f, 0f);
         transform.localPosition = _positionFrom + _direction.GetHalfVector();
-        _progressFactor = 1f / (Mathf.PI * 0.25f);
+        _progressFactor = 1f / (Mathf.PI * 0.5f * (0.5f - _pathOffset));
     }
 
     private void PrepareTurnLeft()
     {
         _directionAngleTo = _directionAngleFrom - 90f;
-        _model.localPosition = new Vector3(0.5f, 0f);
+        _model.localPosition = new Vector3(_pathOffset + 0.5f, 0f);
         transform.localPosition = _positionFrom + _direction.GetHalfVector();
-        _progressFactor = 1f / (Mathf.PI * 0.25f);
+        _progressFactor = 1f / (Mathf.PI * 0.5f * (0.5f - _pathOffset));
     }
 
     private void PrepareTurnAround()
     {
-        _directionAngleTo = _directionAngleFrom + 180f;
-        _model.localPosition = Vector3.zero;
+        _directionAngleTo = _directionAngleFrom + (_pathOffset < 0f ? 180f : -180f);
+        _model.localPosition = new Vector3(_pathOffset, 0f);
         transform.localPosition = _positionFrom;
-        _progressFactor = 2f;
+        _progressFactor = 1f/(Mathf.PI * Mathf.Max(Mathf.Abs(_pathOffset), 0.2f));
     }
 }
 
